2df82c5452c07702c70e027d2619a318dfc10173
Andris Kucins <you@example.com>
2023-05-01 08:50:21 +0300

    generate cod

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Введите элементы массива через пробел:  ");
+        string input = Console.ReadLine() ?? "0";
+
+        string[] inputArr = input.Split(" ");
+        string[] outputArr = new string[inputArr.Length];
+        int outputIndex = 0;
+
+        foreach (var item in inputArr) {
+            if (item.Length <= 3) {
+                outputArr[outputIndex++] = $"\"{item}\"";
+            }
+        }
+
+        Array.Resize(ref outputArr, outputIndex);
+        
+        Console.WriteLine($"Результат: [{string.Join(", ", outputArr)}]");
