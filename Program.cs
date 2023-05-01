1094630c5c3188b5f727738aa2580ff00270b666
Andris Kucins <you@example.com>
2023-05-01 23:31:48 +0300

    add blokshema files png and txt format

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
 ﻿
-Console.Write("Введите элементы массива через пробел:  ");
-        string input = Console.ReadLine() ?? "0";
+// Console.Write("Введите элементы массива через пробел:  ");
+//         string input = Console.ReadLine() ?? "0";
 
-        string[] inputArr = input.Split(" ");
-        string[] outputArr = new string[inputArr.Length];
-        int outputIndex = 0;
+//         string[] inputArr = input.Split(" ");
+//         string[] outputArr = new string[inputArr.Length];
+//         int outputIndex = 0;
 
-        foreach (var item in inputArr) {
-            if (item.Length <= 3) {
-                outputArr[outputIndex++] = $"\"{item}\"";
-            }
-        }
+//         foreach (var item in inputArr) {
+//             if (item.Length <= 3) {
+//                 outputArr[outputIndex++] = $"\"{item}\"";
+//             }
+//         }
 
-        Array.Resize(ref outputArr, outputIndex);
+//         Array.Resize(ref outputArr, outputIndex);
         
-        Console.WriteLine($"Результат: [{string.Join(", ", outputArr)}]");
+//         Console.WriteLine($"Результат: [{string.Join(", ", outputArr)}]");
